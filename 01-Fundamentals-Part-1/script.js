6cace559f9019d7b01cf222103ab94a207f73e7f
Ghost <75405213+AbhilashChutia@users.noreply.github.com>
2022-10-03 16:52:42 +0530

    Add files via upload

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -26,6 +26,8 @@ console.log(firstName);
  */
 
 //Conventions for variable names
+
+/* 
 let jonas_matilda = 'JM';
 let $function = 27;
 
@@ -41,6 +43,7 @@ let job1 = 'Programmer';
 let job2 = 'Professor';
 
 console.log(myFirstJob);
+*/
 
 //-----------------------------------------------------------------------------------//
 //Data Types - Boolean
@@ -129,6 +132,7 @@ console.log(now - 1999 > now - 2018); */
 //-----------------------------------------------------------------------------------//
 // Operator Precedence
 
+/* 
 const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
@@ -141,6 +145,7 @@ console.log(x,y);
 
 const averageAge = (ageJonas + ageSarah) / 2;
 console.log(ageJonas, ageSarah, averageAge);
+*/
 
 //-----------------------------------------------------------------------------------//
 //Template Literals
@@ -171,6 +176,7 @@ lines`)
 //-----------------------------------------------------------------------------------//
 //Taking Decisions
 
+/* 
 const age = 15;
 
 if(age >= 18) {
@@ -190,3 +196,134 @@ if(birthYear <= 2000) {
     century = 21;
 }
 console.log(century);
+*/
+
+//-----------------------------------------------------------------------------------//
+//Type Conversion
+
+/* 
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23); 
+*/
+
+//Type Coercion
+
+/* 
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3)
+console.log('23' / '2');
+*/
+
+/* 
+let n = '1' + 1; // '11'
+n = n - 1;
+console.log(n); 
+*/
+
+//-----------------------------------------------------------------------------------//
+//Truthy and Falsy Values
+
+//5 Falsy values
+//0, '', undefined, null, NaN
+
+/* 
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean('Jonas'));
+console.log(Boolean(''));
+console.log(Boolean({ }));
+
+const money = 100;
+if(money) {
+    console.log("Don't spend it all!");
+} else {
+    console.log('You should get a job!');
+}
+
+let height = 0;
+if (height) {
+    console.log('Yay! Height is defined');
+} else {
+    console.log('Height is Undefined');
+}
+*/
+
+
+//-----------------------------------------------------------------------------------//
+//Equality Operators
+
+/* 
+const age = '18';
+if (age === 18) {
+    console.log('You just became an adult! (Strict Equality)');
+}
+
+if (age == 18) {
+    console.log('You just became an adult! (Loose Equality)')
+}
+
+// === Strict Equality operator - No Type Coercion
+// == Loose Equality Operator - Type Coersion
+*/
+
+//Prompt
+
+/* 
+const favourite = Number(prompt("What's your favourite number? :"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) {
+    console.log("Cool! 23 is an amazing number!");
+} else if (favourite === 7) {
+    console.log("Cool! 7 is an amazing number!");
+} else if (favourite === 9) {
+    console.log("Cool! 9 is an amazing number!");
+} else {
+    console.log('Number is not 23 or 7 or 9');
+}
+*/
+
+//Not Equal Operator
+
+/* 
+if (favourite !==23) {
+    console.log('Why not 23');
+}
+*/
+
+//-----------------------------------------------------------------------------------//
+//Boolean Logic
+
+//-----------------------------------------------------------------------------------//
+//Logical Operators
+
+// const hasDriversLicense = true; // A
+// const hasGoodVision = true; // B
+
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(!hasDriversLicense);
+
+// if(hasDriversLicense && hasGoodVision) {
+//     console.log('Sarah is able to drive!');
+// } else {
+//     console.log('Someone else should drive.')
+// }
+
+// const isTired = false; // C
+// console.log(hasDriversLicense && hasGoodVision && isTired);
+
+// if(hasDriversLicense && hasGoodVision && !isTired) {
+//     console.log('Sarah is able to drive!');
+// } else {
+//     console.log('Someone else should drive.')
+// }
+
