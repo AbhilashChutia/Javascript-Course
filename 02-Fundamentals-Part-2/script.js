71eab1863091321d8daa83528906323ae09cc4f6
Ghost <75405213+AbhilashChutia@users.noreply.github.com>
2022-10-15 23:54:59 +0530

    Add files via upload

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -90,4 +90,88 @@ console.log(yearsUntilRetirement(1980, 'Bob'));
 */
 
 //-----------------------------------------------------------------------------------//
+//Function Calling Other Function
+
+/* 
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitprocessors(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges`;
+    return juice;
+}
+console.log(fruitprocessors(2,3));
+*/
+
+//-----------------------------------------------------------------------------------//
+//Reviewing Functions
+
+/* 
+const calcAge = function(birthYear) {
+    return 2037 - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement}`);  
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired`);
+        return -1;
+    }
+
+    //return `${firstName} retires in ${retirement} years`;
+}
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1970, 'Mike'));
+*/
+
+//-----------------------------------------------------------------------------------//
+// Introduction to Arrays
+
+/* 
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+const years = new Array(1991, 1994, 2008, 2020);
+console.log(years);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends.length - 1);
+
+friends[2] = 'Jay';
+console.log(friends);
+
+const firstName = 'Jonas';
+const Jonas = [firstName, 'Schmidtmann', 2037 - 1991, 'teacher', friends];
+console.log(Jonas);
+console.log(Jonas.length);
+*/
+
+// Exercise
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
